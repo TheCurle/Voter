894f7a60be13bea7eee9256165b437b90d184fcb
Curle <curle@gemwire.uk>
2021-10-06 20:12:36 +0100

    Initial files.

---
+++ src/main/java/uk/gemwire/voter/BotMain.java
@@ -0,0 +1,23 @@
+package uk.gemwire.voter;
+
+
+/**
+ * The entry point for the Voter bot.
+ *
+ * The Voter bot itself allows the management of a repository (such as text-based documentation) to be placed into the hands
+ * of the community.
+ *
+ * If a Pull Request gathers more than a (configurable) majority of positive votes, it will be merged automatically.
+ * If a Pull Request gathers more than a majority of negative votes, it will be closed.
+ *
+ * If a Pull Request reaches its full term without a majority of votes, it will be assigned to maintainers to make the final decision.
+ *
+ * All in all, a simple bot.
+ */
+public class BotMain {
+
+
+    public static void main(String[] args) {
+
+    }
+}
